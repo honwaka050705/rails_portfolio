1e1f5f197335958b9c3d4f61eaebda923ec2ff5f
k-naito <projecthonwaka@gmail.com>
2023-07-23 14:10:54 +0900

    Javascript実装準備

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import jquery from "jquery"
+window.$ = jquery
+import("./custom/common.js")
+
+console.log('application.jsからお試しで表示');
