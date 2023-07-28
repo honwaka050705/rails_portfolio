42699cbfc4a60cd79bc8c42e16b646c0b0834a68
k-naito <projecthonwaka@gmail.com>
2023-07-28 19:29:06 +0900

    step2-001_元号取得_設定

--- app/controllers/conversion_pages_controller.rb
+++ app/controllers/conversion_pages_controller.rb
@@ -1,13 +1,20 @@
 class ConversionPagesController < ApplicationController
+  def personal
+  end
+  
   def emperor
     # TODO DBをpersonalのyeasのカラムに合わせる。Schemaに書き込んで以下のコマンドを実行。
     # `bundle exec rails ridgepole:apply`
     # TODO 2023/07/27(木)はSelectBoxに返却する値を取得して返却する。
+    emperors = Year.all
+    respond_to do |format|
+      format.html
+      format.json { render json: emperors }
+    end
   end
   
-  def personal
-    years = { generation: 126, emperor_name: '令和', ad: 2019 }
-    debugger
+  def to_ad
+    years = Year.find_by(generation: params[:emperor].to_i)
     # TODO moduleで処理する
     @ret_years = { emperor: years[:generation], 
                   nippons_year: params[:nippons_year], 
--- app/javascript/custom/personal.js
+++ app/javascript/custom/personal.js
@@ -1,15 +1,22 @@
 $(function(){
-  $(".emperor").
+  $.ajax({
+    type: 'GET',
+    url: '/emperor',
+    dataType: 'json'
+  }).done(function(data) {
+    $.each(data, function(index, val) {
+      $(".emperor").append($('<option>').attr({ value: val.generation }).text(val.emperor_name))
+    })
+  })
 });
 
 $(".nippons_year").on("blur", function() {
   $.ajax({
     type: 'GET',
-    url: '/',
+    url: '/to_ad',
     data: { emperor: $(".emperor").val(), nippons_year: $(this).val() },
     dataType: 'json'
   }).done(function(data) {
-    console.log(data);
-    $(".chg_ad").text(data.chg_ad + "年");
+    $(".chg_ad").val(data.chg_ad);
   })
 });
