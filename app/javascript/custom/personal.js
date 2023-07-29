ac45f99df5f9fbc95c38232daa36961dca612bff
k-naito <projecthonwaka@gmail.com>
2023-07-29 11:19:12 +0900

    step2-002_スタイルの微調整

--- app/javascript/custom/personal.js
+++ app/javascript/custom/personal.js
@@ -5,18 +5,21 @@ $(function(){
     dataType: 'json'
   }).done(function(data) {
     $.each(data, function(index, val) {
-      $(".emperor").append($('<option>').attr({ value: val.generation }).text(val.emperor_name))
+      $("#emperor").append($('<option>').attr({ value: val.generation }).text(val.emperor_name))
     })
   })
 });
 
-$(".nippons_year").on("blur", function() {
+$("#nippons_year").on("blur", function() {
+  $("#chg_ad").val("");
   $.ajax({
     type: 'GET',
     url: '/to_ad',
-    data: { emperor: $(".emperor").val(), nippons_year: $(this).val() },
+    data: { emperor: $("#emperor").val(), nippons_year: $(this).val() },
     dataType: 'json'
   }).done(function(data) {
-    $(".chg_ad").val(data.chg_ad);
+    $("#chg_ad").text(data.chg_ad + "年");
+    console.log("chg_adのtext:");
+    console.log($("#chg_ad").text());
   })
 });
